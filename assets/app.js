58eadc0cc66cd29b3c346db01279ed7f37f225c4
Nitin Varghese <nitin@pix.city>
2023-10-20 11:43:13 +0530

    updates

--- assets/app.js
+++ assets/app.js
@@ -13,11 +13,13 @@ const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
+import 'bootstrap'
 
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
 
 $(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
+    console.log('aaaa')
+    // $('[data-toggle="popover"]').popover();
 });
