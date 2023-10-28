2236361a15ca04c412fc2d1d44a9a2dd80f38cf4
Nitin Varghese <nitin@pix.city>
2023-10-28 10:56:52 +0530

    airport details

--- assets/app.js
+++ assets/app.js
@@ -10,16 +10,65 @@ require('./styles/app.scss');
 import './styles/app.scss';
 
 const $ = require('jquery');
+import 'select2';
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
-import 'bootstrap'
+import 'bootstrap';
+
 
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
 
 $(document).ready(function() {
-    console.log('aaaa')
-    // $('[data-toggle="popover"]').popover();
+    console.log('aaaa');
+
+    if($('.search-flight').length) {
+        console.log('found');
+        var url = $('#flight-from').parent('.search-flight').data('fetch-airport-url');
+
+        $('#flight-from').select2({
+            width: '300px',
+            minimumInputLength: 1, // Minimum number of characters required to start searching
+            ajax: {
+                url: url, // The URL to your PHP script
+                dataType: 'json',  // Expected data type (json in this case)
+                delay: 20,        // Delay in milliseconds before sending the request
+                processResults: function (data) {
+                    console.log('Request sent');
+                    // Process the data returned from the server
+                    return {
+                        results: data
+                    };
+                }
+            }
+        });
+
+        $('#flight-to').select2({
+            width: '300px',
+            minimumInputLength: 1, // Minimum number of characters required to start searching
+            ajax: {
+                url: url, // The URL to your PHP script
+                dataType: 'json',  // Expected data type (json in this case)
+                delay: 20,        // Delay in milliseconds before sending the request
+                processResults: function (data) {
+                    console.log('Request sent');
+                    // Process the data returned from the server
+                    return {
+                        results: data
+                    };
+                }
+            }
+        });
+
+        $('.search-flight').on('submit', function (e) {
+            console.log('submit');
+            e.preventDefault();
+            var airportFrom = $('#flight-from').val();
+            var airportTo = $('#flight-to').val();
+            var _date = $('#datepicker').val();
+            console.log(airportFrom, airportTo, _date);
+        });
+    }
 });
