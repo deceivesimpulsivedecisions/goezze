2b013c393c791f2dd556013635e0a43e93aad2d9
nitinvarghese829 <nitinvarghese829@gmail.com>
2024-07-25 21:30:21 +0530

    contact us

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\ContactUs;
+use App\Form\ContactUsFormType;
 use App\Repository\DestinationRepository;
 use App\Repository\PackageCategoryRepository;
 use App\Repository\PackageRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,6 +25,30 @@ class DefaultController extends AbstractController
             'destinations' => $destinations,
             'categories' => $categories,
             'packages' => $packages
+        ]);
+    }
+
+    #[Route('/contact-us', name: 'contactUs')]
+    public function contactUs(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $enquiry = new ContactUs();
+        $form = $this->createForm(ContactUsFormType::class, $enquiry);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Save the enquiry to the database or perform any action
+            // For example:
+            $entityManager->persist($enquiry);
+            $entityManager->flush();
+
+            // Add a flash message or redirect to a 'thank you' page
+            $this->addFlash('success', 'Enquiry submitted successfully!');
+
+            return $this->redirectToRoute('contactUs');
+        }
+        return $this->render('default/contactUs.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
